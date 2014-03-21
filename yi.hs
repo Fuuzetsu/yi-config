ee1b69adbe72e9d407565f31cdbf28813dd8062e
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-03-21 20:02:02 +0000

    Truncate whitespace on save

--- yi.hs
+++ yi.hs
@@ -1,5 +1,8 @@
-import           Control.Applicative ((<|>))
+{-# LANGUAGE UnicodeSyntax #-}
+
+import           Control.Applicative ((<|>), (*>), Applicative)
 import           Control.Lens ((%~))
+import           Control.Monad
 import           Data.Bits
 import           Yi
 import           Yi.FuzzyOpen
@@ -13,18 +16,32 @@ import           Yi.Mode.Haskell.Utils (ghciInsertMissingTypes,
 import           Yi.Mode.Haskell.Utils.PastePipe (lpasteCustom)
 import           Yi.Monad (gets)
 
+before ∷ Applicative f ⇒ f a → f b → f a
+before f g = g *> f
+
+after ∷ Applicative f ⇒ f a → f b → f b
+after f g = f *> g
+
+around ∷ Applicative f ⇒ f a → f b → f b
+around f g = g *> f *> g
+
 myModeTable :: [AnyMode]
 myModeTable =
   [ AnyMode $ haskellModeHooks Haskell.preciseMode
   ] ++ modeTable defaultEmacsConfig
 
 
-myKeymap :: KeymapSet
+myKeymap ∷ KeymapSet
 myKeymap = Emacs.mkKeymap $ override Emacs.defKeymap $ \proto _self ->
    proto {
-           Emacs.eKeymap = Emacs.eKeymap proto
-                           <|> (ctrlCh 'c' ?>> ctrlCh 'f' ?>>! fuzzyOpen)
+           Emacs.eKeymap =
+              Emacs.eKeymap proto
+              ||> (ctrlCh 'x' ?>> ctrlCh 's' ?>>! saveAndTruncate)
+              <|> (ctrlCh 'c' ?>> ctrlCh 'f' ?>>! fuzzyOpen)
          }
+
+saveAndTruncate ∷ YiM ()
+saveAndTruncate = before fwriteE $ withBuffer deleteTrailingSpaceB
 
 haskellModeHooks :: Mode syntax -> Mode syntax
 haskellModeHooks mode =
